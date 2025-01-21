7080e4de89bdfc378befe83070c1074973acf6a0
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2025-01-21 16:53:20 -0500

    Update Batch Calculator.bat

--- Batch Calculator.bat
+++ Batch Calculator.bat
@@ -2,13 +2,14 @@
 setlocal
 title Batch Calculator
 echo Program Name: Batch Calculator
-echo Version: 2.0.13
+echo Version: 3.0.0
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
 echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli
 set History=off
 set Equation=
+set Entry=
 goto "CheckExist"
 
 :"CheckExist"
@@ -27,7 +28,7 @@ goto "CheckExist"
 echo EQUATION IS RESTRICTED TO 32-BITS! MAKE SURE YOU DO NOT EXCEED 32-BITS IN ANY PART OF THE EQUATION!
 echo.
 echo Saving equation history is %History%.
-echo Current equation: %Equation%=
+echo Current equation: %Equation%%Entry%
 echo.
 echo [1] Enter digit(s).
 echo.
@@ -40,18 +41,20 @@ echo.
 echo [7] Opening parentheses.
 echo [8] Closing parentheses.
 echo.
-echo [9] Calculate equation.
-echo [10] Clear equation.
+echo [9] Equals.
 echo.
-echo [11] Show equation history.
-echo [12] Turn off saving equation history.
-echo [13] Turn on saving equation history.
-echo [14] Clear equation history.
+echo [10] Clear entry.
+echo [11] Clear.
 echo.
-echo [15] Close.
+echo [12] Show equation history.
+echo [13] Turn off saving equation history.
+echo [14] Turn on saving equation history.
+echo [15] Clear equation history.
+echo.
+echo [16] Close.
 echo.
 set Input=
-set /p Input="What do you want to do? (1-15) "
+set /p Input="What do you want to do? (1-16) "
 if /i "%Input%"=="1" goto "1"
 if /i "%Input%"=="2" goto "2"
 if /i "%Input%"=="3" goto "3"
@@ -67,6 +70,7 @@ if /i "%Input%"=="12" goto "12"
 if /i "%Input%"=="13" goto "13"
 if /i "%Input%"=="14" goto "14"
 if /i "%Input%"=="15" goto "15"
+if /i "%Input%"=="16" goto "16"
 echo Invalid syntax!
 echo.
 goto "Start"
@@ -74,84 +78,110 @@ goto "Start"
 :"1"
 echo.
 set /p Number="Enter digit(s). "
-set Equation=%Equation%%Number%
+set Equation=%Equation%%Entry%
+set Entry=%Number%
 set Number=
 cls
 goto "Start"
 
 :"2"
-set Equation=%Equation%+
+set Equation=%Equation%%Entry%
+set Entry=+
 cls
 goto "Start"
 
 :"3"
-set Equation=%Equation%-
+set Equation=%Equation%%Entry%
+set Entry=-
 cls
 goto "Start"
 
 :"4"
-set Equation=%Equation%*
+set Equation=%Equation%%Entry%
+set Entry=*
 cls
 goto "Start"
 
 :"5"
-set Equation=%Equation%/
+set Equation=%Equation%%Entry%
+set Entry=/
 cls
 goto "Start"
 
 :"6"
-set Equation=%Equation%%%
+set Equation=%Equation%%Entry%
+set Entry=%%
 cls
 goto "Start"
 
 :"7"
-set Equation=%Equation%(
+set Equation=%Equation%%Entry%
+set Entry=(
 cls
 goto "Start"
 
 :"8"
-set Equation=%Equation%)
+set Equation=%Equation%%Entry%
+set Entry=)
 cls
 goto "Start"
 
 :"9"
 echo.
-set /a Result=%Equation%
-if "%errorlevel%"=="0" echo %Equation%=%Result%
+set /a Result=%Equation%%Entry%
+if "%errorlevel%"=="0" echo %Equation%%Entry%=%Result%
 if not "%errorlevel%"=="0" goto "Error"
-if /i "%History%"=="on" (echo %Equation%=%Result%) >> "History.txt"
+if /i "%History%"=="on" (echo %Equation%%Entry%=%Result%) >> "History.txt"
 echo Press any key to continue.
 set Result=
 pause > nul 2>&1
 set Equation=
+set Current=
 cls
 goto "Start"
 
 :"Error"
-echo Press any key to continue.
 ver > nul 2>&1
 set Result=
-pause > nul 2>&1
+set Entry=
 set Equation=
+echo Press any key to continue.
+pause > nul 2>&1
 cls
 goto "Start"
 
 :"10"
 echo.
-set ClearEquation=
-set /p ClearEquation="Are you sure you want to clear your equation %Equation%=? (Yes/No) "
-if /i "%ClearEquation%"=="Yes" goto "ClearEquation"
-if /i "%ClearEquation%"=="No" cls
-if /i "%ClearEquation%"=="No" goto "Start"
+set ClearEntry=
+set /p ClearEntry="Are you sure you want to clear your last entry of %Entry%? (Yes/No) "
+if /i "%ClearEntry%"=="Yes" goto "Clear"
+if /i "%ClearEntry%"=="No" cls
+if /i "%ClearEntry%"=="No" goto "Start"
 echo Invalid syntax!
 goto "10"
 
-:"ClearEquation"
-set Equation=
+:"ClearEntry"
+set Entry=
 cls
 goto "Start"
 
 :"11"
+echo.
+set ClearEquation=
+set /p ClearEquation="Are you sure you want to clear your equation of %Equation%%Entry%? (Yes/No) "
+if /i "%ClearEquation%"=="Yes" goto "ClearEquation"
+if /i "%ClearEquation%"=="No" cls
+if /i "%ClearEquation%"=="No" goto "Start"
+echo Invalid syntax!
+goto "11"
+
+:"ClearEquation"
+set Entry=
+set Equation=
+cls
+goto "Start"
+
+:"12"
 if exist "History.txt" echo.
 if exist "History.txt" type "History.txt"
 if not exist "History.txt" echo There is no history to show! Press any key to continue.
@@ -160,7 +190,7 @@ pause > nul 2>&1
 cls
 goto "Start"
 
-:"12"
+:"13"
 if "%History%"=="On" goto "Off"
 echo History is already off! Press any key to continue.
 pause > nul 2>&1
@@ -172,7 +202,7 @@ set History=off
 cls
 goto "Start"
 
-:"13"
+:"14"
 if "%History%"=="off" goto "On"
 echo History is already on! Press any key to continue.
 pause > nul 2>&1
@@ -184,7 +214,7 @@ set History=on
 cls
 goto "Start"
 
-:"14"
+:"15"
 if exist "History.txt" goto "SureClear"
 echo There is no history to clear! Press any key to continue.
 pause > nul 2>&1
@@ -205,7 +235,7 @@ del "History.txt" /f /q > nul 2>&1
 cls
 goto "Start"
 
-:"15"
+:"16"
 echo.
 set Close=
 set /p Close="Are you sure you want to close this batch file? Closing this batch file will automaticly clear your equation history. (Yes/No) "
